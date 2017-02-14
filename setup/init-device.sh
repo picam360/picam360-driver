5ba9862a32c89099302d4fdab54e21716240d3ea
takumadx <hirayama.takuma@ohmydigifab.com>
2017-02-14 16:09:54 +0900

    shell

--- setup/init-device.sh
+++ setup/init-device.sh
@@ -1,10 +1,15 @@
+CAMERA_WIDTH=2048
+CAMERA_HEIGHT=2048
+#CAMERA_WIDTH=1440
+#CAMERA_HEIGHT=1440
+
 # cam0
-/usr/bin/raspivid -cd MJPEG -t 0 -co 20 -w 2048 -h 2048 -fps 5 -cs 0 -b 8000000 -o - | /usr/bin/socat - UDP-DATAGRAM:192.168.40.2:9100 &
-#/usr/bin/raspivid -ih -t 0 -ex sports -w 1440 -h 1440 -fps 30 -cs 0 -b 2000000 -o - | /usr/bin/socat - UDP-DATAGRAM:192.168.40.2:9100 &
-#/usr/bin/raspivid -cd MJPEG -t 0 -ex sports -w 2048 -h 2048 -fps 5 -cs 0 -b 8000000 -o - | nc -l 9100 &
+/usr/bin/raspivid -cd MJPEG -t 0 -co 20 -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -fps 5 -cs 0 -b 8000000 -o - | /usr/bin/socat - UDP-DATAGRAM:192.168.40.2:9100 &
+#/usr/bin/raspivid -ih -t 0 -ex sports -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -fps 30 -cs 0 -b 2000000 -o - | /usr/bin/socat - UDP-DATAGRAM:192.168.40.2:9100 &
+#/usr/bin/raspivid -cd MJPEG -t 0 -ex sports -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -fps 5 -cs 0 -b 8000000 -o - | nc -l 9100 &
 
 # cam1
-/usr/bin/raspivid -cd MJPEG -t 0 -co 20 -w 2048 -h 2048 -fps 5 -cs 1 -b 8000000 -o - | /usr/bin/socat - UDP-DATAGRAM:192.168.40.2:9101 &
+/usr/bin/raspivid -cd MJPEG -t 0 -co 20 -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -fps 5 -cs 1 -b 8000000 -o - | /usr/bin/socat - UDP-DATAGRAM:192.168.40.2:9101 &
 
 #wait for i2c available
 sleep 3
