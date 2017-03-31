218bfee9903569db772b4e431e3d37ae3cbfcf73
takumadx <hirayama.takuma@ohmydigifab.com>
2017-03-31 14:42:05 +0900

    mrevent

--- rtp.cc
+++ rtp.cc
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <string>
+#include <list>
 
 #include "rtpsession.h"
 #include "rtpudpv4transmitter.h"
@@ -26,6 +27,17 @@
 
 #include "rtp.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mrevent.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+
 using namespace jrtplib;
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
@@ -35,7 +47,15 @@ static bool lg_receive_run = false;
 pthread_t lg_receive_thread;
 static RTPSession lg_sess;
 static pthread_mutex_t lg_mlock = PTHREAD_MUTEX_INITIALIZER;
+
 static int lg_record_fd = -1;
+pthread_t lg_record_thread;
+static MREVENT_T lg_record_packet_ready;
+static pthread_mutex_t lg_record_packet_queue_mlock = PTHREAD_MUTEX_INITIALIZER;
+static std::list<RTPPacket*> lg_record_packet_queue;
+
+static int lg_load_fd = -1;
+pthread_t lg_load_thread;
 
 static RTP_CALLBACK lg_callback = NULL;
 
@@ -59,11 +79,11 @@ int rtp_sendpacket(unsigned char *data, int data_len, int pt) {
 		gettimeofday(&time, NULL);
 		struct timeval diff;
 		timersub(&time, &last_time, &diff);
-		int diff_nsec = diff.tv_sec * 1000000 + (float) diff.tv_usec;
-		if (diff_nsec == 0) {
-			diff_nsec = 1;
+		int diff_usec = diff.tv_sec * 1000000 + (float) diff.tv_usec;
+		if (diff_usec == 0) {
+			diff_usec = 1;
 		}
-		status = lg_sess.SendPacket(data, data_len, pt, false, diff_nsec);
+		status = lg_sess.SendPacket(data, data_len, pt, false, diff_usec);
 		checkerror(status);
 		last_time = time;
 	}
@@ -82,17 +102,19 @@ static void *receive_thread_func(void* arg) {
 				RTPPacket *pack;
 
 				while ((pack = lg_sess.GetNextPacket()) != NULL) {
-					if (lg_callback) {
+					if (lg_callback && lg_load_fd < 0) {
 						lg_callback(pack->GetPayloadData(),
 								pack->GetPayloadLength(),
 								pack->GetPayloadType());
 					}
 					if (lg_record_fd > 0) {
-						write(lg_record_fd, pack->GetPacketData(),
-								pack->GetPacketLength());
+						pthread_mutex_lock(&lg_record_packet_queue_mlock);
+						lg_record_packet_queue.push_back(pack);
+						mrevent_trigger(&lg_record_packet_ready);
+						pthread_mutex_unlock(&lg_record_packet_queue_mlock);
+					} else {
+						lg_sess.DeletePacket(pack);
 					}
-
-					lg_sess.DeletePacket(pack);
 				}
 			} while (lg_sess.GotoNextSourceWithData());
 		}
@@ -103,6 +125,113 @@ static void *receive_thread_func(void* arg) {
 		status = lg_sess.Poll();
 		checkerror(status);
 #endif // RTP_SUPPORT_THREAD
+	}
+	return NULL;
+}
+
+static void *record_thread_func(void* arg) {
+	RTPPacket *pack;
+	while (lg_record_fd >= 0) {
+		int res = mrevent_wait(&lg_record_packet_ready, 1000);
+		if (res != 0) {
+			continue;
+		}
+		int fd = lg_record_fd;
+		if (fd < 0) { //for thread safe
+			continue;
+		}
+
+		pthread_mutex_lock(&lg_record_packet_queue_mlock);
+		pack = *(lg_record_packet_queue.begin());
+		lg_record_packet_queue.pop_front();
+		if (lg_record_packet_queue.empty()) {
+			mrevent_reset(&lg_record_packet_ready);
+		}
+		pthread_mutex_unlock(&lg_record_packet_queue_mlock);
+
+		unsigned short len = pack->GetPacketLength();
+		unsigned char len_bytes[2];
+		for (int i = 0; i < 2; i++) {
+			len_bytes[i] = (len >> (8 * i)) & 0xFF;
+		}
+		write(fd, len_bytes, 2);
+		write(fd, pack->GetPacketData(), len);
+		lg_sess.DeletePacket(pack);
+	}
+	pthread_mutex_lock(&lg_record_packet_queue_mlock);
+	while (!lg_record_packet_queue.empty()) {
+		pack = *(lg_record_packet_queue.begin());
+		lg_record_packet_queue.pop_front();
+		lg_sess.DeletePacket(pack);
+	}
+	mrevent_reset(&lg_record_packet_ready);
+	pthread_mutex_unlock(&lg_record_packet_queue_mlock);
+	return NULL;
+}
+
+static void *load_thread_func(void* arg) {
+	RTP_LOADING_CALLBACK callback = (RTP_LOADING_CALLBACK) arg;
+	unsigned char buff[RTP_MAXPAYLOADSIZE + sizeof(struct RTPHeader)];
+	unsigned int last_timestanp = 0;
+	struct timeval last_time = { };
+	bool is_first = true;
+	int ret = 0;
+	while (lg_load_fd >= 0) {
+		int read_len;
+		struct RTPHeader *header = (struct RTPHeader *) buff;
+		read_len = read(lg_load_fd, buff, 2);
+		if (read_len != 2) {
+			//error
+			break;
+		}
+		unsigned short len = 0;
+		for (int i = 0; i < 2; i++) {
+			len += (buff[i] << (8 * i));
+		}
+		if (len > sizeof(buff)) {
+			//error
+			ret = -1;
+			break;
+		}
+		read_len = read(lg_load_fd, buff, len);
+		if (read_len != len) {
+			//error
+			ret = -1;
+			break;
+		}
+
+		if (lg_callback) {
+			lg_callback(buff + sizeof(struct RTPHeader),
+					len - sizeof(struct RTPHeader), header->payloadtype);
+		}
+		{ //wait
+			unsigned int timestamp = ntohl(header->timestamp);
+			struct timeval time = { };
+			gettimeofday(&time, NULL);
+			if (is_first) {
+				is_first = false;
+				last_timestanp = timestamp;
+				gettimeofday(&last_time, NULL);
+			}
+			int elapsed_nsec;
+			if (timestamp < last_timestanp) {
+				elapsed_nsec = timestamp + (UINT_MAX - last_timestanp);
+			} else {
+				elapsed_nsec = timestamp - last_timestanp;
+			}
+			struct timeval diff;
+			timersub(&time, &last_time, &diff);
+			int diff_nsec = diff.tv_sec * 1000000 + (float) diff.tv_usec;
+
+			if (diff_nsec < elapsed_nsec) {
+				usleep(MIN(elapsed_nsec - diff_nsec, 1000000));
+			}
+			last_time = time;
+			last_timestanp = timestamp;
+		}
+	}
+	if (callback) {
+		callback(ret);
 	}
 	return NULL;
 }
@@ -133,7 +262,8 @@ int init_rtp(unsigned short portbase, char *destip_str,
 	RTPSessionParams sessparams;
 
 	sessparams.SetOwnTimestampUnit(1.0 / 1E6);	//micro sec
-	sessparams.SetMaximumPacketSize(RTP_MAXPAYLOADSIZE + 32);
+	sessparams.SetMaximumPacketSize(
+	RTP_MAXPAYLOADSIZE + sizeof(struct RTPHeader));
 	sessparams.SetAcceptOwnPackets(true);
 	transparams.SetPortbase(portbase);
 
@@ -148,6 +278,8 @@ int init_rtp(unsigned short portbase, char *destip_str,
 	lg_receive_run = true;
 	pthread_create(&lg_receive_thread, NULL, receive_thread_func, (void*) NULL);
 
+	mrevent_init(&lg_record_packet_ready);
+
 	return 0;
 }
 
@@ -161,13 +293,33 @@ int deinit_rtp() {
 	is_init = false;
 	return 0;
 }
+
 void rtp_start_recording(char *path) {
 	rtp_stop_recording();
 	lg_record_fd = open(path, O_CREAT | O_WRONLY | O_TRUNC);
+	pthread_create(&lg_record_thread, NULL, record_thread_func, (void*) NULL);
 }
+
 void rtp_stop_recording() {
 	if (lg_record_fd > 0) {
-		close (lg_record_fd);
+		int fd = lg_record_fd;
 		lg_record_fd = -1;
+		pthread_join(lg_record_thread, NULL);
+		close(fd);
+	}
+}
+
+void rtp_start_loading(char *path, RTP_LOADING_CALLBACK callback) {
+	rtp_stop_loading();
+	lg_load_fd = open(path, O_RDONLY);
+	pthread_create(&lg_load_thread, NULL, load_thread_func, (void*) callback);
+}
+
+void rtp_stop_loading() {
+	if (lg_load_fd > 0) {
+		int fd = lg_load_fd;
+		lg_load_fd = -1;
+		pthread_join(lg_load_thread, NULL);
+		close(fd);
 	}
 }
