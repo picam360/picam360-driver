78a0a21f3c0d263747ea9de39fed8e78fb4441e3
takumadx <hirayama.takuma@ohmydigifab.com>
2017-04-10 11:44:04 +0900

    bandwidthlimit & buffering

--- lunch.sh
+++ lunch.sh
@@ -33,6 +33,23 @@ mkfifo cmd
 chmod 0666 cmd
 
 
+if [ -e rtp_rx ]; then
+	rm rtp_rx
+fi
+mkfifo rtp_rx
+chmod 0666 rtp_rx
+
+if [ -e rtp_tx ]; then
+	rm rtp_tx
+fi
+mkfifo rtp_tx
+chmod 0666 rtp_tx
+
+sudo killall socat
+#socat -u udp-recv:9004 - > rtp_rx &
+socat PIPE:rtp_tx UDP-DATAGRAM:192.168.4.2:9002 &
+socat tcp-connect:192.168.4.2:9002 PIPE:rtp_tx &
+	
 # cam0
 /usr/bin/raspivid -cd MJPEG -t 0 -co 20 -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -fps 5 -cs $CAM0 -b 8000000 -o - > cam0 &
 #/usr/bin/raspivid -ih -t 0 -ex sports -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -fps 30 -cs $CAM0 -b 2000000 -o - > cam0 &
